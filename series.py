158e9221074e1a788777966bdc0780bea60bb97a
yifeiliang <zx245455600@gmail.com>
2017-11-21 10:35:45 -0600

    Version 1.0

---
+++ series.py
@@ -0,0 +1,16 @@
+'''Read and print an interger series'''
+
+import sys
+
+def read_series(filename):
+    with open(filename, model = 'rt', encoding = 'utf-8') as f:
+        return [ int(line.strip()) for line in f]
+
+
+
+def main(filename):
+    series = read_series(filename)
+    print(series)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
